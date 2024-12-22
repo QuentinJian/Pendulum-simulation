2b0cc815663a9b518bd618e4e4c8e9b6b0017cb0
JIAN-NB\clare <quentin.jian@mrandmrsjian.com>
2024-12-22 23:04:17 +0800

    Finished Interactive Script And Draw the phase diagram

--- main.c
+++ main.c
@@ -15,7 +15,11 @@ typedef struct {
     double domega; 
 } Derivative;
 
-Derivative evaluate(State state, double length) {
+Derivative evaluate(State state, double length, double dt, Derivative k) {
+    State temp = {
+        .theta = state.theta + k.dtheta * dt,
+        .omega = state.omega + k.domega * dt
+    };
     Derivative output;
     output.dtheta = state.omega;
     output.domega = -(G / length) * sin(state.theta);
@@ -25,21 +29,10 @@ Derivative evaluate(State state, double length) {
 void RK4(State *state, double dt, double length) {
     State initial = *state;
     
-    Derivative k1 = evaluate(initial, length);
-    
-    State temp = {
-        .theta = initial.theta + (k1.dtheta * dt/2),
-        .omega = initial.omega + (k1.domega * dt/2)
-    };
-    Derivative k2 = evaluate(temp, length);
-    
-    temp.theta = initial.theta + (k2.dtheta * dt/2);
-    temp.omega = initial.omega + (k2.domega * dt/2);
-    Derivative k3 = evaluate(temp, length);
-    
-    temp.theta = initial.theta + (k3.dtheta * dt);
-    temp.omega = initial.omega + (k3.domega * dt);
-    Derivative k4 = evaluate(temp, length);
+    Derivative k1 = evaluate(initial, length, dt, (Derivative){0, 0});
+    Derivative k2 = evaluate(initial, length, dt/2, k1);
+    Derivative k3 = evaluate(initial, length, dt/2, k2);
+    Derivative k4 = evaluate(initial, length, dt, k3);
     
     state->theta = initial.theta + (dt/6.0) * 
         (k1.dtheta + 2*k2.dtheta + 2*k3.dtheta + k4.dtheta);
@@ -48,7 +41,7 @@ void RK4(State *state, double dt, double length) {
 }
 
 void simulate_pendulum(double init_theta, double init_omega,const char* filename, const char* store_folder, double length, double dt, double total_time) {
-    State state = {init_theta, 0};
+    State state = {init_theta, init_omega};
     double t = 0.0;
     char path[100];
     sprintf(path, "%s/%s", store_folder, filename);
@@ -62,6 +55,11 @@ void simulate_pendulum(double init_theta, double init_omega,const char* filename
     fprintf(data, "TIME,ANGLE,ANGULAR_VELOCITY\n");
     while (t < total_time) {
         fprintf(data, "%.2f,%.6f,%.6f\n", t, state.theta, state.omega);
+        if (state.theta > 2*M_PI) {
+            state.theta = -state.theta;
+        } else if (state.theta < -2*M_PI) {
+            state.theta = -state.theta;
+        }
         RK4(&state, dt, length);
         t += dt;
     }
@@ -71,14 +69,14 @@ void simulate_pendulum(double init_theta, double init_omega,const char* filename
 int main() {
     double length = 1.0;
     double dt = 0.01;
-    double total_time = 10.0;
+    double total_time = 5;
     int angle_deg;
     
-    for (double theta = M_PI/8; theta <= 2*M_PI; theta += M_PI/8) {
+    for (double theta = M_PI/6; theta <= 2*M_PI; theta += M_PI/4) {
         angle_deg = (int)(theta * 180.0 / M_PI);
         char filename[100];
         snprintf(filename, sizeof(filename), "pendulum_simulation_%d_deg.csv", angle_deg);
-        simulate_pendulum(theta, 0, filename, "SimulationResultTest", length, dt, total_time);
+        simulate_pendulum(theta, 3, filename, "SimulationResultO2", length, dt, total_time);
     }
     return 0;
 }
